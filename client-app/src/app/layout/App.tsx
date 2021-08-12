172e4eff8197f32594c7cee770746cae545a61c8
Robin Bette <robinbette@hotmail.com>
2021-08-12 13:08:57 +0200

    Delete and clean up

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,30 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { observer } from "mobx-react-lite";
 import { Container } from "semantic-ui-react";
 import { useStore } from "../stores/store";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import { Activity } from "../models/activity";
 import NavBar from "./NavBar";
-import agent from "../api/agents";
 import LoadingComponent from "./LoadingComponent";
 
 function App() {
   const { activityStore } = useStore();
 
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [submitting, setSubmitting] = useState(false);
-
   useEffect(() => {
     activityStore.loadActivities();
   }, [activityStore]);
-
-  const handleDelete = (id: string) => {
-    setSubmitting(true);
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter((x) => x.id !== id)]);
-      setSubmitting(false);
-    });
-  };
 
   if (activityStore.loadingInitial) return <LoadingComponent content="Loading app" />;
 
@@ -32,11 +19,7 @@ function App() {
     <>
       <NavBar />
       <Container style={{ marginTop: "7em" }}>
-        <ActivityDashboard
-          activities={activityStore.activities}
-          deleteActivity={handleDelete}
-          submitting={submitting}
-        />
+        <ActivityDashboard />
       </Container>
     </>
   );
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,16 +1,11 @@
+import { observer } from "mobx-react-lite";
 import { SyntheticEvent, useState } from "react";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
-import { Activity } from "../../../app/models/activity";
 import { useStore } from "../../../app/stores/store";
 
-interface Props {
-  activities: Activity[];
-  deleteActivity: (id: string) => void;
-  submitting: boolean;
-}
-
-export default function ActivityList({ activities, deleteActivity, submitting }: Props) {
+export default observer(function ActivityList() {
   const { activityStore } = useStore();
+  const { activities, deleteActivity, loading } = activityStore;
 
   const [target, setTarget] = useState("");
   const handleActivityDelete = (e: SyntheticEvent<HTMLButtonElement>, id: string) => {
@@ -41,7 +36,7 @@ export default function ActivityList({ activities, deleteActivity, submitting }:
                 />
                 <Button
                   name={activity.id}
-                  loading={submitting && target === activity.id}
+                  loading={loading && target === activity.id}
                   onClick={(e) => handleActivityDelete(e, activity.id)}
                   floated="right"
                   content="Delete"
@@ -55,4 +50,4 @@ export default function ActivityList({ activities, deleteActivity, submitting }:
       </Item.Group>
     </Segment>
   );
-}
+});
