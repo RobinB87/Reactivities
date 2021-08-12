a381138e7c0fc23e4105a35618abe2e38a0a8f5b
Robin Bette <robinbette@hotmail.com>
2021-08-12 10:36:32 +0200

    Add persisting

--- client-app/src/app/api/agents.ts
+++ client-app/src/app/api/agents.ts
@@ -30,6 +30,10 @@ const requests = {
 
 const Activities = {
   list: () => requests.get<Activity[]>("/activities"),
+  details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+  create: (activity: Activity) => requests.post<void>("/activities", activity),
+  update: (activity: Activity) => requests.put<void>(`/activities/${activity.id}`, activity),
+  delete: (activity: Activity) => requests.del<void>(`/activities/${activity.id}`),
 };
 
 const agent = {
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,6 +14,7 @@ interface Props {
   closeForm: () => void;
   createOrEdit: (activity: Activity) => void;
   deleteActivity: (id: string) => void;
+  submitting: boolean;
 }
 
 export default function ActivityDashboard({
@@ -26,6 +27,7 @@ export default function ActivityDashboard({
   closeForm,
   createOrEdit,
   deleteActivity,
+  submitting,
 }: Props) {
   return (
     <Grid>
@@ -40,7 +42,14 @@ export default function ActivityDashboard({
             openForm={openForm}
           />
         )}
-        {editMode && <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+        {editMode && (
+          <ActivityForm
+            closeForm={closeForm}
+            activity={selectedActivity}
+            createOrEdit={createOrEdit}
+            submitting={submitting}
+          />
+        )}
       </Grid.Column>
     </Grid>
   );
