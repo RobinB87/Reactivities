490ebc0f8a20a4900c29d8e29118c8505e51d323
Robin Bette <robinbette@hotmail.com>
2021-09-07 13:58:21 +0200

    finalize date input

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -6,6 +6,7 @@ import { createBrowserHistory } from "history";
 
 import "react-calendar/dist/Calendar.css";
 import "react-toastify/dist/ReactToastify.min.css";
+import "react-datepicker/dist/react-datepicker.css";
 import "./app/layout/styles.css";
 import "semantic-ui-css/semantic.min.css";
 import reportWebVitals from "./reportWebVitals";
