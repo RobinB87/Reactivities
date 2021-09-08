c1038be0e3150aebed5165f3063d31901d6a4f51
Robin Bette <robinbette@hotmail.com>
2021-09-08 17:04:16 +0200

    add new infrastructure project for 'cross-cutting concerns' create IUserAccessor

---
+++ Infrastructure/Security/UserAccessor.cs
@@ -0,0 +1,22 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Security.Claims;
+
+namespace Infrastructure.Security
+{
+    public class UserAccessor : IUserAccessor
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserAccessor(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+        }
+
+        public string GetUserName()
+        {
+            return _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+        }
+    }
+}
